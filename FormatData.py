f9912629991769ec8d585812cf0667c18d37f24f
Alex Morgan <alexmorgan2020@me.com>
2019-07-03 11:55:09 -0400

    V3

--- FormatData.py
+++ FormatData.py
@@ -1,22 +1,31 @@
 import pyreadr
 import pandas as pd
 
-data = pyreadr.read_r('/Users/amorgan/Documents/AgricultureModel/Datasets/01.AgriculturalExtensionServices.RData')
+
+data = pyreadr.read_r('./Datasets/01.AgriculturalExtensionServices.RData')
 agExtServices = data['table']
 
-data = pyreadr.read_r('/Users/amorgan/Documents/AgricultureModel/Datasets/02.AgriculturalProduction_level_1.RData')
+data = pyreadr.read_r('./Datasets/02.AgriculturalProduction_level_1.RData')
 agProdLvl1 = data['table']
 
-data = pyreadr.read_r('/Users/amorgan/Documents/AgricultureModel/Datasets/'
-                      '03.AgriculturalProduction_plot_repeat_begin_level_2.RData')
+data = pyreadr.read_r('./Datasets/03.AgriculturalProduction_plot_repeat_begin_level_2.RData')
 agProdLvl2 = data['table']
 
-data = pyreadr.read_r('/Users/amorgan/Documents/AgricultureModel/Datasets/04.AgriculturalProduction_crop_level_3.RData')
+data = pyreadr.read_r('./Datasets/04.AgriculturalProduction_crop_level_3.RData')
 agProdLvl3 = data['table']
 
-data = pyreadr.read_r('/Users/amorgan/Documents/AgricultureModel/Datasets/'
-                      '05.AgriculturalProduction_group_crop_i_croptype_level_4.RData')
+data = pyreadr.read_r('./Datasets/05.AgriculturalProduction_group_crop_i_croptype_level_4.RData')
 agProdLvl4 = data['table']
 
-data = pyreadr.read_r('/Users/amorgan/Documents/AgricultureModel/Datasets/06.AgriculturalSubsidyCard.RData')
+data = pyreadr.read_r('./Datasets/06.AgriculturalSubsidyCard.RData')
 agSubsidyCard = data['table']
+
+data = pyreadr.read_r('./Datasets/33.FoodDiary_level_1.RData')
+
+'''data = pyreadr.read_r('./Datasets/34.FoodDiary_food_level_2.RData')
+foodDiaryLvl2 = data['table']
+
+data = pyreadr.read_r('./Datasets/35.Fooddiary_food_foodtype_level_3.RData')
+foodDiaryLvl3 = data['table']'''
+
+
