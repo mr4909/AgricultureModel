e1d304a8ae5be0a958f7e6487d6a61ebf905cfad
Alex Morgan <alexmorgan2020@me.com>
2019-07-04 14:30:46 -0400

    V3

--- FormatData.py
+++ FormatData.py
@@ -16,3 +16,8 @@ cropsLvl4 = pd.read_stata('./Datasets/15.Crops_group_crop_i_croptype_level_4.dta
 foodDiaryLvl1 = pd.read_stata('./Datasets/33.FoodDiary_level_1.dta')
 foodDiaryLvl2 = pd.read_stata('./Datasets/34.FoodDiary_food_level_2.dta')
 foodDiaryLvl3 = pd.read_stata('./Datasets/35.Fooddiary_food_foodtype_level_3.dta')
+
+foodDiaryLvl1.drop(['submissiondate', 'start_time', 'end_time', 'today', 'start_date', 'expiration_date',
+                    'max_submissions', 'task_value', 'task_length'], axis=1, inplace=True)
+foodDiaryLvl2.drop(['food_label'], axis=1, inplace=True)
+foodDiaryLvl3.drop(['food_type_label', 'food_unit_label'], axis=1, inplace=True)
