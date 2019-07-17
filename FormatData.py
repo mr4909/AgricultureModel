6b5be695ef1874ae67ce1ed5445e3da819bab8b8
Alex Morgan <alexmorgan2020@me.com>
2019-07-16 22:00:16 -0400

    V8

--- FormatData.py
+++ FormatData.py
@@ -12,6 +12,9 @@ import pandas as pd
 # cropsLvl3 = pd.read_stata('./Datasets/14.Crops_crop_level_3.dta')
 # cropsLvl4 = pd.read_stata('./Datasets/15.Crops_group_crop_i_croptype_level_4.dta')
 
+hhCompLvl1 = pd.read_stata('./Datasets/HouseholdComposition_level_1.dta')
+hhCompLvl2 = pd.read_stata('./Datasets/HouseholdComposition_members_level_2.dta')
+
 fDiaryLvl1 = pd.read_stata('./Datasets/33.FoodDiary_level_1.dta').sort_values(by=['hh_ID']).reset_index(drop=True)
 fDiaryLvl2 = pd.read_stata('./Datasets/34.FoodDiary_food_level_2.dta')
 fDiaryLvl3 = pd.read_stata('./Datasets/35.Fooddiary_food_foodtype_level_3.dta')
@@ -33,22 +36,21 @@ fDiary2Link3 = fDiary1Link2.merge(fDiaryLvl3).sort_values(by=['hh_ID']).reset_in
 foodsList = pd.Series(fDiary2Link3.food_type_name.unique())
 # The list of different foods which will be used to calculate calories
 
-groupedIDAndWeek = fDiary2Link3.groupby(['hh_ID', 'week_number']).sum()
+groupedIDAndWeek = fDiary2Link3.groupby(['hh_ID', 'week_number'], as_index=False).sum()
 
 householdIDLvl1 = pd.Series(fDiaryLvl1.hh_ID.unique())
 householdIDLinked = pd.Series(fDiaryLvl1.hh_ID.unique()).sort_values()
 weekNumbers = pd.Series(fDiaryLvl1.week_number.unique()).sort_values()
+unitsUsed = pd.Series(fDiaryLvl3.food_type_unit.unique())
 
 responseTest = pd.DataFrame(columns=weekNumbers, index=householdIDLinked)
 responseTest = responseTest.fillna(0)
-# If there is a hh_ID, find its week number and count that as a 1, otherwise there was no response
-# Ideally, everything is a 0 or a 3
-
+# If there is a hh_ID, find its week number and count that, otherwise there was no response
+# Set it so the lvl1 gives a 1, the linked gives a 2, and both of them gives a 3
+# Does not work as intended because there are multiple responses per week
 for y in fDiaryLvl1.index:
     responseTest.at[fDiaryLvl1.loc[y, 'hh_ID'], fDiaryLvl1.loc[y, 'week_number']] += 1
 
 for x in fDiary2Link3.index:
     responseTest.at[fDiary2Link3.loc[x, 'hh_ID'], fDiary2Link3.loc[x, 'week_number']] += 2
 
-
-# Set it so the lvl1 gives a 1, the linked gives a 2, and both of them gives a 3
