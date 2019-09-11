9f79ddb05fb23e7b6057ad0f601a2c61c55be613
Alex Morgan <alexmorgan2020@me.com>
2019-09-11 19:34:27 -0400

    V17: Sorted (without the household members)

--- FormatData.py
+++ FormatData.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# hhCompLvl1 = pd.read_stata('./Datasets/HouseholdComposition_level_1.dta')
-# hhCompLvl2 = pd.read_stata('./Datasets/HouseholdComposition_members_level_2.dta')
+hhCompLvl1 = pd.read_stata('./Datasets/HouseholdComposition_level_1.dta')
+hhCompLvl2 = pd.read_stata('./Datasets/HouseholdComposition_members_level_2.dta')
 
 fDiaryLvl1 = pd.read_stata('./Datasets/33.FoodDiary_level_1.dta').sort_values(by=['hh_ID']).reset_index(drop=True)
 fDiaryLvl2 = pd.read_stata('./Datasets/34.FoodDiary_food_level_2.dta').sort_values(by=['hh_ID', 'food_1_ID',
@@ -33,29 +33,28 @@ unitsUsed = pd.Series(fDiaryLvl3.food_type_unit.unique())
 foodsList = pd.DataFrame(fDiary2Link3.food_type_name.unique(), columns=['food_type'])
 # The list of different foods which will be used to calculate calories
 foodGroups = pd.Series(fDiary2Link3.food_grp_namec.unique())
-# TODO: Find the calories for each food
-# Conversion from weight to volume is not the same for each food type. Depending on the food, different units are used
 foodsCalories = pd.read_excel('FoodCalories.xlsx', sheet_name='Sheet1')
 # Calories for each food
 foodsCalories = foodsCalories.fillna(0)
 missingCalories = foodsCalories[foodsCalories.calories == 0]
-# TODO: Create a dataframe that has a household and 1 group for each food type by week (so it's hh_ID vs week)
 
 
 foodGroupsHousehold = pd.DataFrame(columns=weekNumbers, index=householdIDLinked)
 foodGroupsHousehold = foodGroupsHousehold.fillna(0)
 grpFDiary2Link3 = fDiary2Link3.groupby(['hh_ID', 'food_grp_namec', 'week_number'], as_index=False).sum()
 # Adds the amount of food for each food group by week and hh_ID
-# TODO: Find which unit is common for each type of food, then create another column for units
+
 mask = fDiary2Link3.crowdsource == 'Yes'
 noCrowdSource = fDiary2Link3[mask]
 crowdSource = fDiary2Link3[~mask]
-# TODO: How to then split these up by crowdsource and not crowdsource?
-# byWeekCrowd = fDiary2Link3[(fDiary2Link3.recall == 'week') and (fDiary2Link3.crowdsource == 'Yes')]
-# Above line does not work?
-byWeek = fDiary2Link3[fDiary2Link3.recall == 'week']
-byMonth = fDiary2Link3[fDiary2Link3.recall == 'month']
-bySeason = fDiary2Link3[fDiary2Link3.recall == 'season']
+
+cSWeekly = crowdSource[crowdSource.recall == 'week']
+cSMonthly = crowdSource[crowdSource.recall == 'month']
+cSSeasonal = crowdSource[crowdSource.recall == 'season']
+
+noCSWeekly = noCrowdSource[noCrowdSource.recall == 'week']
+noCSMonthly = noCrowdSource[noCrowdSource.recall == 'month']
+noCSSeasonal = noCrowdSource[noCrowdSource.recall == 'season']
 
 
 def food_group_sums(food_group, df):
@@ -78,7 +77,6 @@ dairyGroup = dairyGroup.sort_index(axis=1).transpose()
 
 sns.heatmap(meatEggGroup, cmap='PiYG', vmax=500)
 # plt.show()
-# TODO: Create graphs for other food groups
 # TODO: Look into how the color pallets work
 # TODO: Find a way to differentiate between no response and no food in the graphs
 # TODO: Normalize the graphs so it is food/person in each household instead of food/household
